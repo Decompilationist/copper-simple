5e14b65460168e3dcb516427fd20c7c06c2e3fd4
Gustavo Menezes <103705903+Decompilationist@users.noreply.github.com>
2024-05-06 13:50:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
 
 require('dotenv').config();
 
@@ -71,6 +72,30 @@ app.post('/enviar-email', async (req, res) => {
         console.error('Erro ao enviar e-mail:', error);
         res.status(500).json({ error: 'Erro ao enviar e-mail' });
     }
+});
+
+app.get('/converter/:dollar', async (req, res) => {
+  try {
+    // Obter o valor do parâmetro 'dollar' do caminho da URL
+    const dollar = req.params.dollar;
+
+    // Verificar se o parâmetro 'dollar' é um número válido
+    if (isNaN(parseFloat(dollar))) {
+      throw new Error('Valor de dólar inválido.');
+    }
+
+    // Construir a URL com o valor fornecido para fazer a requisição à API do Banco Central do Brasil
+    const url = `https://www3.bcb.gov.br/bc_moeda/rest/converter/${dollar}/1/220/790/2024-05-06`;
+
+    // Fazer a requisição para a API do Banco Central do Brasil
+    const response = await axios.get(url);
+
+    // Retornar os dados da resposta
+    res.json(response.data);
+  } catch (error) {
+    console.error('Erro ao testar a API do Banco Central do Brasil:', error);
+    res.status(500).send('Erro ao testar a API do Banco Central do Brasil');
+  }
 });
 
 app.listen(PORT, () => {
